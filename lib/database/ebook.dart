4ef3ad4fdfd6b3f6510ed83a098d3cb9a4be3c56
thedrivingforce <thedrivingforc@users.noreply.github.com>
2021-02-23 17:22:51 +0530

    ebook_database added

---
+++ lib/database/ebook.dart
@@ -0,0 +1,87 @@
+//    SEMESTER----------1
+class EC101 {
+  List<Map<String, String>> booksBECE = [
+    {
+      'MILLMAN-HALKIAS':
+          'https://drive.google.com/file/d/1rM7rdxGcPrLI2ddtksYE9Bmt4qeWgv0N/view?usp=sharing',
+      'ROBERT-BOYLSTEAD':
+          'https://drive.google.com/file/d/1dYDYYyd1zgjNCzEm2qCvjujbAbIKl0aI/view?usp=sharing',
+    }
+  ];
+}
+
+class ME101 {
+  List<Map<String, String>> booksBME = [
+    {
+      'Engineering-Mechanics: IRVIN H SHAMES':
+          'https://drive.google.com/file/d/1Bso4KteKbrNGaRtxlB93KPZx8KPC_AFA/view?usp=sharing',
+    }
+  ];
+}
+
+class CH101 {
+  List<Map<String, String>> booksCHEMSISTRY = [
+    {
+      'Organic-Chemistry: MORRISON BOYD':
+          'https://drive.google.com/file/d/1jvVW8pecOdNw8Phm9WCNN5ZrkIq58t6j/view?usp=sharing',
+      'INORGANIC-CHEMISTRY':
+          'https://drive.google.com/file/d/1p1MFrC53GX8ma97qOB4JJ3IeojSGLv8u/view?usp=sharing',
+    }
+  ];
+}
+
+class MA103 {
+  List<Map<String, String>> booksMATHS1 = [
+    {
+      'THOMAS-CALCULAS':
+          'https://drive.google.com/file/d/1OXRiGBlS8BkFrhDExUGdE6WuvzExd8Re/view?usp=sharing',
+      'B.S-GREWAL':
+          'https://drive.google.com/file/d/1vYQZX4NIgWfocXNZkE97yvuWTQwFYFyA/view?usp=sharing',
+      'B.V-RAMANA':
+          'https://drive.google.com/file/d/1vpWze5w3ADo_rVdvjqpmHgbWN69D-JGB/view?usp=sharing',
+    }
+  ];
+}
+
+//    SEMESTER----------2
+class EE101 {
+  List<Map<String, String>> booksEE = [
+    {
+      'HUGHES: Electrical & Electronics Tech':
+          'https://drive.google.com/file/d/1utSZdP-t1uNISekKv_FlMiTukgC4ZlZm/view?usp=sharing',
+      'Dictionary Of EE':
+          'https://drive.google.com/file/d/1GXrOcG0ck0LpH82S_pB2mJsgWkznLdeV/view?usp=sharing',
+    }
+  ];
+}
+
+class MA107 {
+  List<Map<String, String>> booksMATHS2 = [
+    {
+      'H.K-DASS':
+          'https://drive.google.com/file/d/10sJu-R_v_c_BsmuSK9RXVIbj1G2sI5UV/view?usp=sharing',
+    }
+  ];
+}
+
+class CS101 {
+  List<Map<String, String>> booksPPS = [
+    {
+      'Prog in ANSI-C':
+          'https://drive.google.com/file/d/1a5NGFN-RbM2TX6Vxqd_Vq5-3YXWTAl2G/view?usp=sharing',
+      'C Notes for Professionals':
+          'https://drive.google.com/file/d/1tKt6aoz-vu3PuxauAcDmh25tP2WcPndw/view?usp=sharing',
+    }
+  ];
+}
+
+class PH113 {
+  List<Map<String, String>> booksPHYSICS = [
+    {
+      'OPTICS-Ajoy Ghatak':
+          'https://drive.google.com/file/d/1BWtxSFRZ67XKKJN3tiXMXp4E65yttC81/view?usp=sharing',
+    }
+  ];
+}
+
+//! to access------------>>>> BECE.forEach((element) => print(element["THOMAS-CALCULUS"]));
