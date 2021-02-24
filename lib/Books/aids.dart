1ab0ba94ee7598751dee0c889ce8e0b1aa9bf5e0
pdpratyush16 <pratyush.dev.16@gmail.com>
2021-02-24 15:00:37 +0530

    Video database + conflicts resolved

--- lib/Books/aids.dart
+++ lib/Books/aids.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_material_app/Videos/module.dart';
 import 'package:study_material_app/database/subjectCodeDatabase.dart';
 import 'package:study_material_app/Animation/CustomWidgets.dart';
 
@@ -34,6 +35,11 @@ class Aids extends StatelessWidget {
               ),
               CustomTileDesign(
                 name: "VIDEO LINK",
+                onPressed: () {
+                  Navigator.pushNamed(context, ModulePage.id, arguments: {
+                    'Subject': subject,
+                  });
+                },
               ),
 
               //Temporary Display....
